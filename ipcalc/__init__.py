1e40e24fad7b7e2ce094b03f47e05a3555fb42eb
Kalle Møller Aagaard (KM) <git@k-moeller.dk>
2020-06-05 23:27:36 +0200

    iface

--- ipcalc/__init__.py
+++ ipcalc/__init__.py
@@ -1,10 +1,15 @@
 import ipaddress
 
 
-def make_subnet(net: ipaddress.IPv6Network, offset: int, prefix: int):
+def make_subnet(net: ipaddress.IPv6Network, offset: int, prefix: int) -> ipaddress.IPv6Network:
     if (net.prefixlen > prefix):
         raise ValueError(f'{prefix} < {net}')
 
     offset = (offset * (1 << (128-prefix))) & (~net.netmask._ip)
 
     return ipaddress.IPv6Network((net.network_address._ip | offset, prefix))
+
+
+def make_iface(net: ipaddress.IPv6Network, offset: int) -> ipaddress.IPv6Interface:
+    offset = offset & (~net.netmask._ip)
+    return ipaddress.IPv6Interface((net.network_address._ip | offset, net.prefixlen))
--- ipcalc/__main__.py
+++ ipcalc/__main__.py
@@ -1,10 +1,13 @@
 import ipaddress
-from . import make_subnet
+from . import make_subnet, make_iface
 
 
 def main():
     prefix = ipaddress.ip_network('2a05:4616:29a7::/48')
-    print(make_subnet(prefix, 0x000c, 64))
+    sub = make_subnet(prefix, 0x000c, 64)
+    print(sub)
+    iface = make_iface(sub, 4)
+    print(iface)
 
 
 if __name__ == '__main__':
